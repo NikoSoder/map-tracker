3647dcaf1829c13f3b8866c32ac282974210b630
Niko <soder.n@gmail.com>
2022-12-14 13:09:01 +0200

    Create add maps page

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -8,17 +9,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AddMapsFormComponent } from './components/add-maps-form/add-maps-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent,
-    AddMapsFormComponent
-  ],
+  declarations: [AppComponent, NavBarComponent, AddMapsFormComponent],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-frontend/src/app/components/add-maps-form/add-maps-form.component.ts
+++ angular-frontend/src/app/components/add-maps-form/add-maps-form.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-maps-form',
   templateUrl: './add-maps-form.component.html',
-  styleUrls: ['./add-maps-form.component.css']
+  styleUrls: ['./add-maps-form.component.css'],
 })
 export class AddMapsFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  userForm = new FormGroup({
+    mapName: new FormControl('', Validators.required),
+    mapStyle: new FormControl('', Validators.required),
+    mapTier: new FormControl('', Validators.required),
+    mapNotes: new FormControl(''),
+    mapCompleted: new FormControl(false, Validators.required),
+  });
+
+  userSubmit() {
+    console.log(this.userForm.value);
+
+    // this.dataService.createData(this.userForm.value).subscribe((res) => {
+    //   console.log(res);
+    // });
+
+    this.userForm.reset();
   }
-
 }
