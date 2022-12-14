a3d529bcf076e3e442e40be42126fb8be05ba341
Niko <soder.n@gmail.com>
2022-12-14 11:48:31 +0200

    Add icons to nav-bar

--- angular-frontend/src/app/components/nav-bar/nav-bar.component.ts
+++ angular-frontend/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { faSquare, faPlusSquare } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faChartLine } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  faSquare = faSquare;
+  faGithub = faGithub;
+  faBars = faBars;
+  faChartLine = faChartLine;
+  faPlusSquare = faPlusSquare;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
