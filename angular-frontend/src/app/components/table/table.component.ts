e4d8a220b59c5746d64c059b601c6b1e9ced9115
Niko <soder.n@gmail.com>
2023-02-09 13:22:33 +0200

    Add sorting to map name and map tier with help of chatGPT

--- angular-frontend/src/app/components/table/table.component.ts
+++ angular-frontend/src/app/components/table/table.component.ts
@@ -8,13 +8,34 @@ import { faSearch, faSort } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
-  @Input() maps!: Map[];
+  sortField = 'name';
+  sortOrder = 'asc';
   searchInput = '';
   faSearch = faSearch;
   faSort = faSort;
+  @Input() maps!: Map[];
   constructor() {}
 
   ngOnInit(): void {}
+
+  sort(field: string) {
+    this.sortField = field;
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+
+    this.maps.sort((a, b) => {
+      if (field === 'map_name') {
+        return this.sortOrder === 'asc'
+          ? a[field].localeCompare(b[field])
+          : b[field].localeCompare(a[field]);
+      } else if (field === 'map_tier') {
+        return this.sortOrder === 'asc'
+          ? a[field] - b[field]
+          : b[field] - a[field];
+      } else {
+        return 0;
+      }
+    });
+  }
 
   onSearch(input: string) {
     this.searchInput = input;
