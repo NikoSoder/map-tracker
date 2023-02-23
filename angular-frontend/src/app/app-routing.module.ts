eed04d8fa455ca96b8d5411ed9a3b80163075276
Niko <soder.n@gmail.com>
2023-02-23 14:45:34 +0200

    Generate landing page component

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { AddMapsFormComponent } from './components/add-maps-form/add-maps-form.c
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { MapDetailComponent } from './components/map-detail/map-detail.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: LandingPageComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: MapDetailComponent },
   { path: 'add-maps', component: AddMapsFormComponent },
