66f363449d24a0a5a2153613f109e975bb93bbac
Niko <soder.n@gmail.com>
2023-02-23 16:02:29 +0200

    Add introduction section to landing page

--- angular-frontend/src/app/components/landing-page/landing-page.component.ts
+++ angular-frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+  faCheck = faCheck;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
