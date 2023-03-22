7995343ab9c1bfedb284a11e9c3b56ee9d65a82b
Niko <soder.n@gmail.com>
2023-03-22 11:58:01 +0200

    Delete add-maps path

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddMapsFormComponent } from './components/add-maps-form/add-maps-form.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { StatisticsComponent } from './components/statistics/statistics.component';
 import { MapDetailComponent } from './components/map-detail/map-detail.component';
@@ -11,7 +10,6 @@ const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: MapDetailComponent },
-  { path: 'add-maps', component: AddMapsFormComponent },
   { path: 'statistics', component: StatisticsComponent },
 ];
 
