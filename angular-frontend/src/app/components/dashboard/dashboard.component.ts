6ca5239d9b079d2e4a43b8ce59dd1078bfdb7bc5
Niko <soder.n@gmail.com>
2023-03-23 13:28:50 +0200

    Remove useless code lines

--- angular-frontend/src/app/components/dashboard/dashboard.component.ts
+++ angular-frontend/src/app/components/dashboard/dashboard.component.ts
@@ -9,16 +9,12 @@ import { Map } from 'src/app/types/map.interface';
 })
 export class DashboardComponent implements OnInit {
   userMaps: Map[] = [];
-  completedMaps: Map[] = [];
-  projectMaps: Map[] = [];
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getAllMaps().subscribe((maps: Map[]) => {
       this.userMaps = maps;
-      this.completedMaps = maps.filter((map) => map.map_completed === 1);
-      this.projectMaps = maps.filter((map) => map.map_completed === 0);
     });
   }
 }
