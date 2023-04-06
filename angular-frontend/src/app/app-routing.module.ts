351cae5ca9a9cadef45cb8ddbdc3c88af898b958
Niko <soder.n@gmail.com>
2023-04-06 13:57:29 +0300

    Remove statistics path

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
 import { MapDetailComponent } from './components/map-detail/map-detail.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
@@ -10,7 +9,6 @@ const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: MapDetailComponent },
-  { path: 'statistics', component: StatisticsComponent },
 ];
 
 @NgModule({
