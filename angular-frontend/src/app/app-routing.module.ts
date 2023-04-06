d49e53d0fe84d3e4317e77fc8cd2c3a5ed98eed4
Niko <soder.n@gmail.com>
2023-04-06 18:33:23 +0300

    Add new map path

--- angular-frontend/src/app/app-routing.module.ts
+++ angular-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MapDetailComponent } from './components/map-detail/map-detail.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { AddMapsFormComponent } from './components/add-maps-form/add-maps-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: LandingPageComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: MapDetailComponent },
+  { path: 'add', component: AddMapsFormComponent },
 ];
 
 @NgModule({
